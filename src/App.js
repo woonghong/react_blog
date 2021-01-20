ee57244095c8299e9f5d1e17625239c19b04057c
wooho <whwin_@naver.com>
2021-01-20 13:24:42 +0900

    props 이용 구조화

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          start <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customer ={
+  'name':'홍길동',
+  'birthday':'961123',
+  'gender':'남자',
+  'job':'대학생'
+}
+
+class App extends Component {
+  render() {
+   return(
+     <Customer
+     name={customer.name}
+     birthday={customer.birthday}
+     gender={customer.gender}
+     job={customer.job}
+     />
+   );
+   }
 }
 
 export default App;
